7daa2462a5c7a5b34a0ca28fdca108f927e49914
hasparus <hasparus@gmail.com>
2020-02-26 14:34:42 +0100

    Add deck

---
+++ theme.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { themes } from "mdx-deck";
+import prism from "@theme-ui/prism/presets/theme-ui";
+import Prism from "@theme-ui/prism";
+
+export const theme = {
+  ...themes.notes,
+  googleFont:
+    "https://fonts.googleapis.com/css?family=Work+Sans:400,700&display=block",
+  fonts: {
+    body: '"Work Sans", sans-serif'
+  },
+  colors: {
+    text: "#000",
+    background: "#fff",
+    primary: "blue",
+    secondary: "#000",
+    accent: "#000",
+    gray: "#666"
+  },
+  styles: {
+    pre: { lineHeight: 1 },
+    code: prism,
+    a: {
+      color: "unset",
+      textDecorationColor: "rgba(0, 50, 255, 0.18)"
+    },
+    li: {
+      margin: "1rem 0",
+      textAlign: "left"
+    },
+    blockquote: {
+      fontStyle: "italic",
+      backgroundColor: "#f8f8f8",
+      padding: "1rem",
+      border: "1px solid black",
+      boxShadow: "0.5rem 0.9rem 0 0.2rem"
+    }
+  },
+  components: {
+    pre: ({ children }) => <>{children}</>,
+    code: Prism
+  }
+};
+
+console.log({ theme });
